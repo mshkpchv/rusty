357ef2bc83c750e1dbae92375eff10579a9bef9f
Mihail <misho>
2022-06-01 23:30:37 +0300

    simple int iterator
    simple int iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 use std::io::{BufRead, BufReader, Read};
-use std::num::ParseIntError;
-use rand::Rng;
 
 pub struct IntLineReader<R> {
     reader: BufReader<R>,
@@ -10,7 +8,6 @@ impl<R: Read> IntLineReader<R> {
     pub fn new(r: BufReader<R>) -> IntLineReader<R> {
         IntLineReader { reader: r }
     }
-
     fn read(&mut self) -> Option<String> {
         let mut buf = String::new();
         let ps = match self.reader.read_line(&mut buf) {
@@ -24,10 +21,9 @@ impl<R: Read> IntLineReader<R> {
                 }
                 return Some(buf)
             }
-            // Err(e) => Some(e),
-            Err(e) => Some(String::from("problem")),
+            Err(_) => Some(String::from("ERROR")),
         };
-        ps
+        return ps
     }
 }
 
@@ -35,51 +31,82 @@ impl<R: Read> Iterator for IntLineReader<R> {
     type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let option = self.read();
-        let rr:Option<i32> = match option {
-            None => None,
-            Some(x) => {
-               // let rr = match x.parse::<i32>() {
-               //      Ok(x) => Some(x),
-               //      Err(_) => None
-               //  };
-               //  rr
-                return Some(x.parse::<i32>().unwrap())
-            }
-        };
-        rr
+            let ss = self.read();
+            let rr:Option<Self::Item> = match ss {
+                None => None,
+                Some(x) => {
+                    return match x.parse::<Self::Item>() {
+                         Ok(x) => Some(x),
+                         Err(_) => return self.next()
+                    };
+                }
+            };
+        return rr
     }
 }
 
-// impl Iterator for IntLine {
-//     type Item = usize;
-//
-//     fn next(&mut self) -> Option<Self::Item> {
-//         if !self.open {
-//             self.reader = BufReader::new(&self.f)
-//         }
-//         let mut line = String::new();
-//         let len = line.len();
-//         return Some(len);
-//     }
-// }
-// impl<B: BufRead> Iterator for IntLine<B> {
-//     type Item = Result<String>;
-//
-//     fn next(&mut self) -> Option<Result<String>> {
-//         let mut buf = String::new();
-//         match self.reader.read_line(&mut buf) {
-//             Ok(0) => None,
-//             Ok(_n) => {
-//                 if buf.ends_with('\n') {
-//                     buf.pop();
-//                     if buf.ends_with('\r') {
-//                         buf.pop();
-//                     }
-//                 }
-//                 Some(Ok(buf))
-//             }
-//             Err(e) => Some(Err(e)),
-//         }
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::io::{BufReader};
+    use crate::IntLineReader;
+
+    #[test]
+    fn it_handles_empty_files() {
+        let file = File::open("test_files/empty_file").unwrap();
+        let mut int_reader = IntLineReader::new(BufReader::new(file));
+
+        assert_eq!(int_reader.next(), None);
+    }
+
+    #[test]
+    fn it_handles_file_with_one_line() {
+        let file = File::open("test_files/one_line_file", ).unwrap();
+        let mut int_reader = IntLineReader::new(BufReader::new(file));
+        assert_eq!(int_reader.next(), Some(1234));
+        assert_eq!(int_reader.next(), None);
+    }
+
+    #[test]
+    fn it_handles_file_with_multi_lines() {
+        let file = File::open("test_files/multi_line_file").unwrap();
+        let mut int_reader = IntLineReader::new(BufReader::new(file));
+
+        assert_eq!(int_reader.next(), Some(1234));
+        assert_eq!(int_reader.next(), Some(1234));
+        assert_eq!(int_reader.next(), Some(1234));
+        assert_eq!(int_reader.next(), Some(1234));
+        assert_eq!(int_reader.next(), None);
+
+    }
+
+    #[test]
+    fn it_handles_file_with_odd_numbers<>() {
+        let file = File::open("test_files/multi_line_odd_file").unwrap();
+        let mut int_reader = IntLineReader::new(BufReader::new(file));
+
+        assert_eq!(int_reader.next(), Some(1));
+        assert_eq!(int_reader.next(), Some(3));
+        assert_eq!(int_reader.next(), Some(5));
+        assert_eq!(int_reader.next(), Some(7));
+        assert_eq!(int_reader.next(), None);
+
+    }
+
+    #[test]
+    fn it_handles_file_with_starting_empty_lines<>() {
+        let file = File::open("test_files/multi_start_lines").unwrap();
+        let mut int_reader = IntLineReader::new(BufReader::new(file));
+        assert_eq!(int_reader.next(), Some(1));
+        assert_eq!(int_reader.next(), None);
+    }
+
+    #[test]
+    fn it_handles_file_with_trailing_lines<>() {
+        let file = File::open("test_files/multi_end_lines").unwrap();
+        let mut int_reader = IntLineReader::new(BufReader::new(file));
+        assert_eq!(int_reader.next(), Some(1));
+        assert_eq!(int_reader.next(), None);
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use intiterator::{IntLineReader};
 
 fn main() {
     let file = File::open("test/foo.txt").unwrap();
-    // BufReader::new(file).lines().filter()
-
     let int_reader = IntLineReader::new(BufReader::new(file));
     for line in int_reader {
         println!("{}", line);
