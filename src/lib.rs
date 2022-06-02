a9b1799313ee537297acb3e9538b3e282595188c
Mihail <misho>
2022-06-02 13:21:44 +0300

    finishing test cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,259 @@
-use std::io::{BufRead, BufReader, Read};
+extern crate core;
 
-pub struct IntLineReader<R> {
-    reader: BufReader<R>,
+use std::cmp::Ordering;
+use std::io::{Error, ErrorKind};
+use itertools::izip;
+use std::num::ParseIntError;
+
+pub struct CorrectLines<I> {
+    iter: I,
 }
 
-impl<R: Read> IntLineReader<R> {
-    pub fn new(r: BufReader<R>) -> IntLineReader<R> {
-        IntLineReader { reader: r }
-    }
-    fn read(&mut self) -> Option<String> {
-        let mut buf = String::new();
-        let ps = match self.reader.read_line(&mut buf) {
-            Ok(0) => None,
-            Ok(_n) => {
-                if buf.ends_with('\n') {
-                    buf.pop();
-                    if buf.ends_with('\r') {
-                        buf.pop();
-                    }
-                }
-                return Some(buf)
-            }
-            Err(_) => Some(String::from("ERROR")),
-        };
-        return ps
+impl<I> CorrectLines<I> {
+    pub fn new(iter: I) -> Self {
+        Self { iter }
     }
 }
 
-impl<R: Read> Iterator for IntLineReader<R> {
-    type Item = i32;
+impl<I> Iterator for CorrectLines<I>
+    where
+        I: Iterator<Item=Result<String, Error>>
+{
+    type Item = Result<i32, ParseIntError>;
 
     fn next(&mut self) -> Option<Self::Item> {
-            let ss = self.read();
-            let rr:Option<Self::Item> = match ss {
-                None => None,
-                Some(x) => {
-                    return match x.parse::<Self::Item>() {
-                         Ok(x) => Some(x),
-                         Err(_) => return self.next()
-                    };
-                }
-            };
-        return rr
+        let ss = self.iter.next();
+        let rr = match ss {
+            Some(x) => {
+                return match x {
+                    Ok(x) => Some(x.parse::<i32>()),
+                    Err(err) => panic!("Problem with reading line {}", err)
+                };
+            }
+            None => None
+        };
+        return rr;
     }
 }
+
+pub trait CorrectLinesIterator<T>: Iterator<Item=T> + Sized {
+    fn to_int(self) -> CorrectLines<Self> {
+        CorrectLines::new(self)
+    }
+}
+
+impl<T, I: Iterator<Item=T>> CorrectLinesIterator<T> for I {}
+
+fn algo<T>(iterator1: T, iterator2: T, skip_val: i32) -> Result<(i32, i32), Error>
+    where T: Iterator<Item=(usize, Result<i32, ParseIntError>)>
+{
+    let iterator2 = iterator2.skip(skip_val as usize);
+
+    let mut current_max = 0;
+    let mut target_i: i32 = -1;
+    let mut target_j: i32 = -1;
+
+    for ((i_line, i_result), (j_line, j_result)) in izip!(iterator1,iterator2) {
+        match (i_result, j_result) {
+            (Ok(l1), Ok(l2)) => {
+                let current_sum = l1 + l2;
+                match current_sum.cmp(&current_max) {
+                    Ordering::Greater => {
+                        target_i = i_line as i32;
+                        target_j = j_line as i32;
+                        current_max = current_sum;
+                    }
+                    _ => {}
+                }
+            }
+            _ => { continue; }
+        }
+    }
+
+    return match (target_i, target_j)
+    {
+        (-1, _) => { Err(Error::from(ErrorKind::InvalidData)) }
+        (_, -1) => { Err(Error::from(ErrorKind::InvalidData)) }
+        (x, y) => Ok((x, y))
+    };
+}
+
 
 #[cfg(test)]
 mod tests {
     use std::fs::File;
-    use std::io::{BufReader};
-    use crate::IntLineReader;
+    use std::io::{BufRead, BufReader, Error, ErrorKind};
+    use std::num::{ParseIntError};
+    use crate::{algo, CorrectLinesIterator};
+
+    fn to_int_result(x: i32) -> Result<i32, ParseIntError> {
+        Ok(x)
+    }
+
+    fn to_string_result(s: &String) -> Result<String, Error> {
+        Ok(s.to_string())
+    }
+
+    #[test]
+    fn test_with_normal_range() {
+        let min = 0;
+        let max = 100;
+        let skip = 3;
+
+        let range1 = (min..max).map(to_int_result).enumerate();
+        let range2 = (min..max).map(to_int_result).enumerate();
+
+        let res = algo(range2, range1, skip);
+
+        assert_eq!(res.unwrap(), (max - skip - 1, max - 1));
+    }
+
+    #[test]
+    fn test_handles_empty_iterator_from_range() {
+        let min = 0;
+        let max = 1;
+        let skip = 3;
+
+        let range1 = (min..max).map(to_int_result).enumerate();
+        let range2 = (min..max).map(to_int_result).enumerate();
+
+        let res = algo(range2, range1, skip);
+        match res {
+            Ok(_) => { panic!() }
+            Err(err) => { assert_eq!(err.kind(), ErrorKind::InvalidData) }
+        }
+    }
+
+    #[test]
+    fn test_with_one_empty_iterator() {
+        let min = 0;
+        let max = 1;
+        let skip = 3;
+
+        let range1 = (10..10000).map(to_int_result).enumerate();
+        let range2 = (min..max).map(to_int_result).enumerate();
+
+        let res = algo(range1, range2, skip);
+        match res {
+            Ok(_) => { panic!() }
+            Err(err) => { assert_eq!(err.kind(), ErrorKind::InvalidData) }
+        }
+    }
+
+    #[test]
+    fn test_handles_small_iterators() {
+        let file = String::from("test_files/success");
+        let f1 = File::open(&file).unwrap();
+        let f2 = File::open(&file).unwrap();
+
+        let skip = 5;
+        let max = 10;
+
+        let range1 = BufReader::new(f1).lines().to_int().enumerate();
+        let range2 = BufReader::new(f2).lines().to_int().enumerate();
+
+        match algo(range1, range2, skip) {
+            Ok(res) => { assert_eq!(res, (max - skip - 1, max - 1)) }
+            Err(_) => { panic!("do not expect it") }
+        }
+    }
+
+    #[test]
+    fn test_handles_iterator_greater_than_max_arr_size() {
+        let min: i32 = 0;
+        let max = i32::MAX / (2 ^ 10);
+        let skip = 1000000;
+        let range1 = (min..max).map(to_int_result).enumerate();
+        let range2 = (min..max).map(to_int_result).enumerate();
+
+        let res = algo(range2, range1, skip);
+
+        assert_eq!(res.unwrap(), (max - skip - 1, max - 1));
+    }
+
+    #[test]
+    fn test_handles_comment_iterators_algo() {
+        let a = vec![String::from("str"), String::from("u123"), String::from("32.22"),
+                     String::from("1.22")];
+        let iterator1 = a.iter().map(to_string_result).to_int().enumerate();
+        let iterator2 = a.iter().map(to_string_result).to_int().enumerate();
+        let skip = 0;
+        match algo(iterator1, iterator2, skip) {
+            Ok(_) => { panic!() }
+            Err(_) => {}
+        }
+    }
+
+    // int iterator tests
 
     #[test]
     fn it_handles_empty_files() {
-        let file = File::open("test_files/empty_file").unwrap();
-        let mut int_reader = IntLineReader::new(BufReader::new(file));
-
-        assert_eq!(int_reader.next(), None);
+        let file = "test_files/empty_file".to_string();
+        let f = File::open(file);
+        let f = match f {
+            Ok(file) => file,
+            Err(error) => panic!("Problem opening the file: {:?}", error),
+        };
+        let mut int_iter = BufReader::new(f).lines().to_int().enumerate();
+        let result = int_iter.next();
+        assert_eq!(result, None);
     }
 
     #[test]
     fn it_handles_file_with_one_line() {
-        let file = File::open("test_files/one_line_file", ).unwrap();
-        let mut int_reader = IntLineReader::new(BufReader::new(file));
-        assert_eq!(int_reader.next(), Some(1234));
-        assert_eq!(int_reader.next(), None);
+        let file = File::open("test_files/one_line_file").unwrap();
+        let mut int_reader = BufReader::new(file).lines().to_int().enumerate();
+        assert_eq!(int_reader.next(), Some((0, Ok(1234))));
+        match int_reader.next() {
+            None => { panic!() }
+            Some((line, el)) => {
+                assert_eq!(line, 1);
+                assert_eq!(el.is_err(), true)
+            }
+        }
+        assert_eq!(int_reader.next(),None)
     }
 
     #[test]
-    fn it_handles_file_with_multi_lines() {
-        let file = File::open("test_files/multi_line_file").unwrap();
-        let mut int_reader = IntLineReader::new(BufReader::new(file));
+    fn test_with_odd_lines_files() {
+        let file = File::open("test_files/odd_lines").unwrap();
+        let int_reader = BufReader::new(file).lines().to_int().enumerate();
+        let mut index = 0;
+        for el in int_reader {
+            let (line, res) = el;
+            assert_eq!(line, index);
+            if index % 2 == 0 {
+                assert_eq!(res.is_ok(), true)
+            } else {
+                assert_eq!(res.is_err(), true)
+            }
 
-        assert_eq!(int_reader.next(), Some(1234));
-        assert_eq!(int_reader.next(), Some(1234));
-        assert_eq!(int_reader.next(), Some(1234));
-        assert_eq!(int_reader.next(), Some(1234));
-        assert_eq!(int_reader.next(), None);
-
+            index = index + 1;
+        }
     }
 
     #[test]
-    fn it_handles_file_with_odd_numbers<>() {
-        let file = File::open("test_files/multi_line_odd_file").unwrap();
-        let mut int_reader = IntLineReader::new(BufReader::new(file));
-
-        assert_eq!(int_reader.next(), Some(1));
-        assert_eq!(int_reader.next(), Some(3));
-        assert_eq!(int_reader.next(), Some(5));
-        assert_eq!(int_reader.next(), Some(7));
-        assert_eq!(int_reader.next(), None);
-
+    fn test_int_adapter_with_vector() {
+        let a = vec![String::from("1"), String::from("1.2")];
+        let mut int_iter = a.iter().map(to_string_result).to_int();
+        let next = int_iter.next();
+        match next {
+            None => panic!(),
+            Some(res) => {
+                match res {
+                    Ok(el) => { assert_eq!(el, 1) }
+                    Err(_) => panic!()
+                }
+            }
+        }
+        let next = int_iter.next();
+        match next {
+            None => panic!(),
+            Some(res) => {
+                assert_eq!(res.is_err(), true)
+            }
+        }
+        let next = int_iter.next();
+        assert_eq!(next, None)
     }
-
-    #[test]
-    fn it_handles_file_with_starting_empty_lines<>() {
-        let file = File::open("test_files/multi_start_lines").unwrap();
-        let mut int_reader = IntLineReader::new(BufReader::new(file));
-        assert_eq!(int_reader.next(), Some(1));
-        assert_eq!(int_reader.next(), None);
-    }
-
-    #[test]
-    fn it_handles_file_with_trailing_lines<>() {
-        let file = File::open("test_files/multi_end_lines").unwrap();
-        let mut int_reader = IntLineReader::new(BufReader::new(file));
-        assert_eq!(int_reader.next(), Some(1));
-        assert_eq!(int_reader.next(), None);
-    }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use std::fs::File;
-use std::io::{BufReader};
-use intiterator::{IntLineReader};
+use std::num::ParseIntError;
+// use intiterator::{algo};
 
 fn main() {
-    let file = File::open("test/foo.txt").unwrap();
-    let int_reader = IntLineReader::new(BufReader::new(file));
-    for line in int_reader {
-        println!("{}", line);
+    // let file = String::from("test_files/success");
+    // let buf = BufReader::new(file);
+    // algo(&file,3);
+    // println!();
+    // let x = [0 as u64; std::u64::MAX as usize];
+    println!("{}", i32::MAX / (2 ^ 25));
+    match String::from("1212").parse::<i32>() {
+        Ok(_) => {}
+        Err(err) => {}
     }
+
+    // algo(&String::from("test_files/"),3);
+
+
 }
 
